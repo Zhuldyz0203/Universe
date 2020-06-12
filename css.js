ead28ffab5942dfcd47b5d557882d59534216f42
Zhuldyz0203 <66788647+Zhuldyz0203@users.noreply.github.com>
2020-06-12 22:09:26 +0600

    Add files via upload

---
+++ css.js
@@ -0,0 +1,49 @@
+body {
+  margin:10;
+  font-family: Arial, Calibri Light , sans-serif;
+  background-size: 10px;
+}
+header{
+	background-image: url(cong-ty-thiet-ke-web-site-chuan-seo.png);
+	background-size: cover;
+}
+menu {
+  overflow: hidden;
+  background-color: #333;
+}
+.menu a {
+
+  color: #000;
+  background-repeat: all;
+  text-align: center;
+  padding: 15px 16px;
+  text-decoration: blink;
+  font-size: 20px;
+  background-color:beige;
+  font-size: 20px;
+  background: rgba(255,255,255,.5);
+  box-shadow: 3px 0 7px rgba(0,0,0,.3);
+  padding: 20px;
+}
+a {
+    border-right: 1px solid #bbb;
+}
+.menu a:hover {
+  background-color: #555;
+  color: #fff;
+  }
+.navbar-logo{
+	background-position: left;
+}
+
+.menu a.active {
+ background-color: #fff;
+ color: black;
+ background: rgba(255,255,255,.5);
+}
+.menu a.active{
+ color: #000;
+ float: right;
+ background-color: #000;
+ background: rgba(255,255,255,.5);
+}
